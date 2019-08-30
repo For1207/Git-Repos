7329d506fa7eb65a45a5590d39dc6e18b1f75381
Igor M <mykhigo@gmail.com>
2019-08-29 21:05:59 -0400

    Timer works
    But doesn't want to stop

--- memory_game/js/app.js
+++ memory_game/js/app.js
@@ -5,6 +5,11 @@ const cards = [...card];
 let cardsOpened = [];
 let cardsMatched = [];
 const deck = document.querySelector('.deck');
+const deckCards = deck.getElementsByClassName('card');
+let movesCounter = 0;
+let clockOff = true;
+let time = 0;
+let clockId;
 
  // shuffle the array of cards
  Array.prototype.shuffle = function() {
@@ -27,35 +32,84 @@ const deck = document.querySelector('.deck');
 
 //add listeners to cards, flip them; match and add to arrays
 
+
 deck.addEventListener('click', event => {
-  const clickTarget = event.target;
-  if (clickTarget.classList.contains('card') &&
-      !clickTarget.classList.contains('match')) {
-      clickTarget.classList.toggle('open');
-      clickTarget.classList.toggle('show');
-      cardsOpened.push(clickTarget);
+const clickTarget = event.target;
 
-      if (cardsOpened.length === 2) {
-        if (cardsOpened[0].firstElementChild.className === cardsOpened[1].firstElementChild.className &&
-           cardsOpened.length <= 2) {
-           cardsOpened[0].classList.add('match');
-           cardsOpened[1].classList.add('match');
-           cardsMatched.unshift(cardsOpened[0], cardsOpened[1]);
-           cardsOpened = [];
-         }
-            else {
-              setTimeout(function() {
-                cardsOpened[0].classList.toggle('open');
-                cardsOpened[0].classList.toggle('show');
-                cardsOpened[1].classList.toggle('open');
-                cardsOpened[1].classList.toggle('show')
-                cardsOpened = [];
-              }, 1000);
-
-         }
-    }
+if (clickTarget.classList.contains('card') &&
+  !clickTarget.classList.contains('match')) {
+  clickTarget.classList.toggle('open');
+  clickTarget.classList.toggle('show');
+  cardsOpened.push(clickTarget);
   }
+// clear array of opened cards
+
+function clearCardsOpened() {
+  for ( let i = 0; i < cardsOpened.length; i++) {
+    cardsOpened[i].classList.toggle('open');
+    cardsOpened[i].classList.toggle('show');
+    }
+  cardsOpened = [];
+  // add moves counter
+  movesCounter = ++movesCounter;
+  document.querySelector('.moves').innerHTML = movesCounter;
+  }
+
+if (cardsOpened.length === 2) {
+if (cards.indexOf(cardsOpened[0]) !== cards.indexOf(cardsOpened[1])) {
+  if (cardsOpened[0].firstElementChild.className === cardsOpened[1].firstElementChild.className) {
+    cardsOpened[0].classList.add('match');
+    cardsOpened[1].classList.add('match');
+    cardsMatched.unshift(cardsOpened[0], cardsOpened[1]);
+    clearCardsOpened();
+    } else {
+        setTimeout(function() {
+        clearCardsOpened()
+        }, 800);
+      }
+  }
+}
+if (cardsOpened.length > 2){
+    clearCardsOpened();
+}
 });
+
+function startClock() {
+  clockId = setInterval(() => {
+  time++;
+  displayTime();
+  }, 1000);
+  }
+
+deck.addEventListener('click', function event() {
+  const clickTarget = event.target;
+    if(clockOff) {
+      startClock();
+      clockOff = false;
+    }
+})
+
+function displayTime() {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const clock = document.querySelector('.clock');
+  if (seconds < 10) {
+  clock.innerHTML = minutes + ' : ' + '0' + seconds;
+} else {
+  clock.innerHTML = minutes + ' : ' + seconds;
+}
+}
+
+function stopClock() {
+  clearInterval(clockId);
+}
+
+let finishGame = cardsMatched.length === 2;
+if (finishGame) {
+  stopClock();
+  console.log (1);
+}
+
 
 /*
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
